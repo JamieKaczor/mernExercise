4510100e61a1a910aa6cdc54ee01cf7dafa1420b
Jamie Kaczor <dignanjk@aol.com>
2021-11-10 15:36:14 -0500

    setting up routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>SkillsBuild Training</h1>
+        <h1>SkillsBuild Training</h1>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import studentRoutes from './routes/student.js';
+
 
 const app = express();
 
-app.use(bodyParser.json({limit: "20mb", extended:true}));
-app.use(bodyParser.urlencoded({limit: "20mb", extended:true}));
+app.use('/students', studentRoutes);
+app.use(bodyParser.json({ limit: "20mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+
 
 app.use(cors());
 
@@ -16,7 +20,7 @@ const CONNECTION_URL = 'mongodb+srv://mongodb:mongodb@cluster0.thxns.mongodb.net
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {
-    useNewUrlParser:true, useUnifiedTopology:true
+    useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => app.listen(PORT, () =>
     console.log(`Connection is established and running on port: ${PORT}`)
 )).catch((err) => console.log(err.message));
